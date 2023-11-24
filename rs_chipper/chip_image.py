4a77eb034767da8cc124294bc4895880bea92218
tomwilsonsco <thomaswilson81@gmail.com>
2023-11-24 14:29:37 +0000

    update chip

--- rs_chipper/chip_image.py
+++ rs_chipper/chip_image.py
@@ -4,12 +4,12 @@ from rasterio.windows import Window
 import numpy as np
 from tqdm import tqdm
 
-def chip_image(input_file_path, output_path, pixel_dimensions=128, offset=64):
+def chip_image(input_image_path, output_path, pixel_dimensions=128, offset=64):
     """
     Split a satellite image into smaller tiles or chips.
 
     Args:
-        input_file_path (str): The file path for the input satellite image.
+        input_image_path (str): The path to the input satellite image.
         output_path (str): The directory path where the output tiles will be saved.
         pixel_dimensions (int, optional): The height and width of each tile in pixels. Defaults to 128.
         offset (int, optional): The offset used when creating tiles, to define the step size. Defaults to 64.
@@ -22,17 +22,17 @@ def chip_image(input_file_path, output_path, pixel_dimensions=128, offset=64):
     The output tiles are named using the base name of the input file with appended
     x and y offsets and saved as TIFF files.
     """
-    print(f"Chipping {input_file_path} to /{output_path}...")
+    print(f"Chipping {input_image_path} to /{output_path}...")
 
     # Convert string paths to Path objects
-    input_file_path = Path(input_file_path)
+    input_image_path = Path(input_image_path)
     output_path = Path(output_path)
 
     # Create output directory if it does not exist
     output_path.mkdir(parents=True, exist_ok=True)
 
     # Calculate the range for x and y to create chips
-    with rio.open(input_file_path) as src:
+    with rio.open(input_image_path) as src:
         for y in tqdm(range(0, src.height, offset), desc="Chipping image..."):
             for x in range(0, src.width, offset):
                 window = Window(x, y, pixel_dimensions, pixel_dimensions)
@@ -44,7 +44,7 @@ def chip_image(input_file_path, output_path, pixel_dimensions=128, offset=64):
                 # Read data into the chip array
                 chip = src.read(window=window, out=chip_data, boundless=True)
 
-                output_file_name = f"{input_file_path.stem}_{x}_{y}.tif"
+                output_file_name = f"{input_image_path.stem}_{x}_{y}.tif"
                 output_file_path = output_path / output_file_name
 
                 with rio.open(
