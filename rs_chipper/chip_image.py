scaling fixes

rs_chipper/chip_image.py
@@ -3,8 +3,20 @@ import rasterio as rio
 from rasterio.windows import Window
 import numpy as np
 from tqdm import tqdm
+from .load_scaler import load_scaler
+from .sample_image_pixels import sample_image_pixels
+from .standard_scale_array import standard_scale_array
 
-def chip_image(input_image_path, output_path, pixel_dimensions=128, offset=64, standard_scale=True, sample_size=10000):
+
+def chip_image(
+    input_image_path,
+    output_path,
+    pixel_dimensions=128,
+    offset=64,
+    standard_scale=True,
+    sample_size=10000,
+    scaler_source=None,
+):
     """
     Split a satellite image into smaller tiles or chips.
 
@@ -14,8 +26,11 @@ def chip_image(input_image_path, output_path, pixel_dimensions=128, offset=64, s
         pixel_dimensions (int, optional): The height and width of each tile in pixels. Defaults to 128.
         offset (int, optional): The offset used when creating tiles, to define the step size. Defaults to 64.
         standard_scale (bool, optional): Whether to standard scale from a sample of pixel values. Defaults to True.
-        sample_size (int, optional): If standard_scale is True, how many pixels are sampled 
-        to derive mean and standard deviation for scaling. 
+        sample_size (int, optional): If standard_scale is True, how many pixels are sampled
+        to derive mean and standard deviation for scaling.
+        scaler_source (str or dict): Optional scaling parameters returned from `sample_image_pixels`. Path
+        to a pickle file or a dictionary with statistics. If not specified these will be derived (default)
+        if standard_scale is True.
 
     Returns:
         None
@@ -23,7 +38,8 @@ def chip_image(input_image_path, output_path, pixel_dimensions=128, offset=64, s
     This function uses rasterio to read a satellite image, then splits the image into
     smaller square tiles of specified dimensions and saves them to the output path.
     The output tiles are named using the base name of the input file with appended
-    x and y offsets and saved as TIFF files.
+    x and y offsets and saved as TIFF files. Optionally the chip pixel values can be standard
+    scaled before saving, using a sample of the full image pixels.
     """
     print(f"Chipping {input_image_path} to /{output_path}...")
 
@@ -34,29 +50,43 @@ def chip_image(input_image_path, output_path, pixel_dimensions=128, offset=64, s
     # Create output directory if it does not exist
     output_path.mkdir(parents=True, exist_ok=True)
 
+    # Create or load standard scaler dictionary if required
+    if standard_scale:
+        if scaler_source is None:
+            scaler_dict = sample_image_pixels(input_image_path, sample_size)
+        else:
+            scaler_dict = load_scaler(scaler_source)
+
     # Calculate the range for x and y to create chips
     with rio.open(input_image_path) as src:
+        d_type = src.dtypes[0]
         for y in tqdm(range(0, src.height, offset), desc="Chipping image..."):
             for x in range(0, src.width, offset):
                 window = Window(x, y, pixel_dimensions, pixel_dimensions)
-                
+
                 transform = src.window_transform(window)
-                
+
                 # Initialize an array for the chip data
-                chip_data = np.zeros((src.count, pixel_dimensions, pixel_dimensions), dtype=src.dtypes[0])
+                chip_data = np.zeros(
+                    (src.count, pixel_dimensions, pixel_dimensions), dtype=src.dtypes[0]
+                )
                 # Read data into the chip array
                 chip = src.read(window=window, out=chip_data, boundless=True)
+                if standard_scale:
+                    chip = standard_scale_array(chip, scaler_dict, src.descriptions)
+                    d_type = chip.dtype
 
                 output_file_name = f"{input_image_path.stem}_{x}_{y}.tif"
                 output_file_path = output_path / output_file_name
 
                 with rio.open(
-                    output_file_path, 'w',
-                    driver='GTiff',
+                    output_file_path,
+                    "w",
+                    driver="GTiff",
                     height=pixel_dimensions,
                     width=pixel_dimensions,
                     count=src.count,
-                    dtype=src.dtypes[0],
+                    dtype=d_type,
                     crs=src.crs,
                     transform=transform,
                 ) as dst:

rs_chipper/sample_image_pixels.py
@@ -1,11 +1,12 @@
+from pathlib import Path
 import numpy as np
 import rasterio
-from pathlib import Path
 import pickle
+
 
 def sample_image_pixels(input_image_path, sample_size=10000):
     """
-    Samples pixel values from an image at random coordinates and calculates the 
+    Samples pixel values from an image at random coordinates and calculates the
     mean and standard deviation for each band.
 
     Args:
@@ -15,7 +16,7 @@ def sample_image_pixels(input_image_path, sample_size=10000):
 
     Returns:
         dict: A dictionary with keys as '<band_name>_mean' and '<band_name>_std'
-        for each band in the image, and values as the calculated mean and 
+        for each band in the image, and values as the calculated mean and
         standard deviation of the sampled pixel values.
     """
     input_image_path = Path(input_image_path)
@@ -27,31 +28,35 @@ def sample_image_pixels(input_image_path, sample_size=10000):
         # Generate random coordinates within the image bounds
         xs = np.random.uniform(left, right, sample_size)
         ys = np.random.uniform(bottom, top, sample_size)
-        
+
         # Prepare a list of coordinates
         coordinates = list(zip(xs, ys))
 
         # Read the pixel values at the coordinates for all bands
         pixel_values = np.array(list(src.sample(coordinates)))
-        
+
         # Get band descriptions
         band_names = src.descriptions
 
         # Calculate mean and standard deviation for each band
         for band_index in range(src.count):
             # Use band description if available, otherwise default to 'Band_<index>'
-            band_name = band_names[band_index] if band_names[band_index] else f'Band_{band_index + 1}'
+            band_name = (
+                band_names[band_index]
+                if band_names[band_index]
+                else f"Band_{band_index + 1}"
+            )
             band_pixel_values = pixel_values[:, band_index]
             valid_band_pixel_values = band_pixel_values[~np.isnan(band_pixel_values)]
-            stats_dict[f'{band_name}_mean'] = np.mean(valid_band_pixel_values)
-            stats_dict[f'{band_name}_std'] = np.std(valid_band_pixel_values)
+            stats_dict[f"{band_name}_mean"] = np.mean(valid_band_pixel_values)
+            stats_dict[f"{band_name}_std"] = np.std(valid_band_pixel_values)
 
     # Create the pickle file path
     pickle_file_name = f"{input_image_path.stem}_{sample_size}.pkl"
     pickle_file_path = input_image_path.with_name(pickle_file_name)
 
     # Save the dictionary to a pickle file
-    with open(pickle_file_path, 'wb') as f:
+    with open(pickle_file_path, "wb") as f:
         pickle.dump(stats_dict, f)
 
     return stats_dict