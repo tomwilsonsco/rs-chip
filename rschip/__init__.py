c111a3c5e4a82d5eaa8b27a5c66f588229b48e6c
tomwilsonsco <17555474+tomwilsonsco@users.noreply.github.com>
2024-10-17 16:54:59 +0100

    test imagechip

---
+++ rschip/__init__.py
@@ -0,0 +1,1 @@
+from rschip.imagechip import ImageChip
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rschip',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'rasterio',
+        'numpy',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+    test_suite='tests',  # This tells setuptools to look in the 'tests' directory for tests
+)
