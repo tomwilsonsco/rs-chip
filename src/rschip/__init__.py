0fdf74e778346f4bddf755873ce387b673368305
tomwilsonsco <17555474+tomwilsonsco@users.noreply.github.com>
2026-03-03 12:07:36 +0000

    module import background

--- src/rschip/__init__.py
+++ src/rschip/__init__.py
@@ -1,3 +1,3 @@
 from rschip.image_chip import ImageChip  # noqa: F401
 from rschip.segmentation_mask import SegmentationMask  # noqa: F401
-from rschip.remove_background_only import CheckBackgroundOnly  # noqa: F401
+from rschip.check_background import CheckBackgroundOnly  # noqa: F401
